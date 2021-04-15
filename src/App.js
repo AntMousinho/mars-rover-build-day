c932cfae539d744e15ca5ad288969e2941ad4364
AntMousinho <antony.mousinho@hotmail.co.uk>
2021-04-15 17:19:41 +0100

    started react router work

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Header from "./components/header/Header.js";
 import MainSubheader from "./components/mainSubheader/MainSubheader.js";
 import Gallery from './components//gallery/Gallery.js'
@@ -6,11 +8,17 @@ import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-	  <MainSubheader />
-	  <Gallery />
-    </div>
+	  <Router>
+		<div className="App">
+			<Header />
+			<Switch>
+				<Route path="/">
+					<MainSubheader />
+					<Gallery />
+				</Route>
+			</Switch>
+		</div>
+	  </Router>
   );
 }
 
